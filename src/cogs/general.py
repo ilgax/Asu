3728b59d7cd838271d391a04724a6601fc4c3e9a
Ilgaz <notilgax@pm.me>
2025-08-30 19:15:01 +0300

    webhook

--- src/cogs/general.py
+++ src/cogs/general.py
@@ -1,3 +1,6 @@
+import asyncio
+
+import aiohttp
 import nextcord
 import random
 from nextcord.ext import commands
@@ -47,10 +50,37 @@ class GeneralCog(commands.Cog):
         result = random.choice(["Heads", "Tails"])
         await interaction.response.send_message(result)
 
-    @nextcord.slash_command(name="counter", guild_ids=[config.GUILD_ID])
+    @nextcord.slash_command(name="counter")
     async def counter(self, interaction: nextcord.Interaction):
         """Starts a counter for pressing."""
         await interaction.send("Press!", view=EphemeralCounter())
+
+    @nextcord.slash_command(name="webhook", guild_ids=[config.GUILD_ID])
+    async def webhook(self, interaction: nextcord.Interaction, url: str, content: str):
+        """Sends a message to a webhook."""
+        if not url.startswith("https://discord.com/api/webhooks/"):
+            await interaction.send("Invalid webhook URL.", ephemeral=True)
+            return
+        if len(content) > 2000:
+            await interaction.send("Message is too long.", ephemeral=True)
+            return
+        await interaction.send(f"Sending message {content!r} to webhook", ephemeral=True)
+        await send_to_webhook(url, content)
+
+    @nextcord.slash_command(guild_ids=[config.GUILD_ID])
+    async def ask(self, interaction):
+        """Asks the user a question to confirm something."""
+        # We create the view and assign it to a variable so we can wait for it later.
+        view = Confirm()
+        await interaction.send("Do you want to continue?", view=view)
+        # Wait for the View to stop listening for input...
+        await view.wait()
+        if view.value is None:
+            print("Timed out...")
+        elif view.value:
+            print("Confirmed...")
+        else:
+            print("Cancelled...")
 
 
 class Counter(nextcord.ui.View):
@@ -78,6 +108,33 @@ class EphemeralCounter(nextcord.ui.View):
     async def receive(self, button: nextcord.ui.Button, interaction: nextcord.Interaction):
         # ephemeral=True makes the message hidden from everyone except the button presser
         await interaction.response.send_message("Enjoy!", view=Counter(), ephemeral=True)
+
+class Confirm(nextcord.ui.View):
+    def __init__(self):
+        super().__init__()
+        self.value = None
+
+    # When the confirm button is pressed, set the inner value to `True` and
+    # stop the View from listening to more input.
+    # We also send the user an ephemeral message that we're confirming their choice.
+    @nextcord.ui.button(label="Confirm", style=nextcord.ButtonStyle.green)
+    async def confirm(self, button: nextcord.ui.Button, interaction: nextcord.Interaction):
+        await interaction.response.send_message("Confirming", ephemeral=True)
+        self.value = True
+        self.stop()
+
+    # This one is similar to the confirmation button except sets the inner value to `False`
+    @nextcord.ui.button(label="Cancel", style=nextcord.ButtonStyle.grey)
+    async def cancel(self, button: nextcord.ui.Button, interaction: nextcord.Interaction):
+        await interaction.response.send_message("Cancelling", ephemeral=True)
+        self.value = False
+        self.stop()
+
+async def send_to_webhook(url, content):
+    # Create a new HTTP session and use it to create webhook object
+    async with aiohttp.ClientSession() as session:
+        webhook = nextcord.Webhook.from_url(url, session=session)
+        await webhook.send(content)
 
 
 def setup(bot):
